eece5b477e4e57ac6c319c29c5fd486ca3d14752
basheera <basheera.k.a@hotmail.com>
2020-02-04 17:28:50 +0300

    calc

--- calculator.py
+++ calculator.py
@@ -1,6 +1,20 @@
 
 def main():
-	#write your code here
+	num1=input("Enter the First number: ")
+	num2=input("Enter the Second number: ")
+	operator=input("Enter the operation: ")
+	if (num1.isdecimal()==False or num2.isdecimal()==False):
+		print("Not Valid numbers!")
+	elif (operator!='+' and operator!='-' and operator!='*' and operator!='/'):
+		print("invalid Operation!")
+	else:
+		if (operator=='+'):
+			print(int(num1)+int(num2))
+		elif(operator=='-'):
+			print(int(num1)-int(num2))
+		elif(operator=='*'):
+			print(int(num1)*int(num2))
+		else: print(int(num1)/int(num2))
 	pass
 	
 
